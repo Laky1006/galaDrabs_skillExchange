19d553e8a5518b5e22ea9819057f451600994832
Anna Hr <ah00238@rvt.lv>
2025-01-15 12:14:45 +0200

    Moving to github

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <p>
+      Times clicked: {{score}}
+    </p>
+    <div>
+      <img src="https://captaincookiedc.com/wp-content/uploads/2023/04/home-icon-cookie.png" 
+      @click="handleClickEvent">
+    </div>
+  </main>
+</template>
+
+
+<script>
+  export default {
+    data(){
+      return {
+        score: 0,
+      }
+    },
+    methods: {
+      handleClickEvent(){
+        this.score++
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  img {
+    height: 500px;
+  }
+
+  div {
+    height: 94vh;
+    width: 98vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
